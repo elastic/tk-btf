dbd3c4674353737abc2099dad8ed29c3c08f2fd8
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2023-12-05 19:32:15 +0200

    fix: add BitFieldTypeMask test with a mask that has more than one zeros

--- fetch_arg_bitfield_test.go
+++ fetch_arg_bitfield_test.go
@@ -8,15 +8,18 @@ import (
 
 func TestBitFieldTypeMask(t *testing.T) {
 
-	bitFieldType := BitFieldTypeMask(uint8(0x4))
+	bitFieldType := BitFieldTypeMask(uint8(0b100))
 	require.Equal(t, "b1@2/8", bitFieldType)
 
-	bitFieldType = BitFieldTypeMask(uint16(0x4))
+	bitFieldType = BitFieldTypeMask(uint16(0b100))
 	require.Equal(t, "b1@2/16", bitFieldType)
 
-	bitFieldType = BitFieldTypeMask(uint32(0x4))
+	bitFieldType = BitFieldTypeMask(uint32(0b100))
 	require.Equal(t, "b1@2/32", bitFieldType)
 
-	bitFieldType = BitFieldTypeMask(uint64(0x4))
+	bitFieldType = BitFieldTypeMask(uint64(0b100))
 	require.Equal(t, "b1@2/64", bitFieldType)
+
+	bitFieldType = BitFieldTypeMask(uint32(0b111100000))
+	require.Equal(t, "b4@5/32", bitFieldType)
 }
