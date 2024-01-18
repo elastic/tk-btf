e6d84908ea2a710b6f28c13e8f180d425ca13b77
Panos Koutsovasilis <panos.koutsovasilis@elastic.co>
2024-01-18 16:31:33 +0200

    feat: license sched example and add collision detection for the base name of produced stripped btfs

--- examples/sched/main.go
+++ examples/sched/main.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package main
 
 import (
@@ -65,7 +82,9 @@ func loadWakeUpNewTaskSymbol(symbolMap map[string]*tkbtf.Symbol) {
 	)
 
 	symbolMap["wake_up_new_task"] = wakeUpNewTaskSymbol
+}
 
+func loadTaskStatsExitSymbol(symbolMap map[string]*tkbtf.Symbol) {
 	taskStatsExitSymbol := tkbtf.NewSymbol("taskstats_exit").AddProbes(
 		tkbtf.NewKProbe().SetRef("taskstats_exit").AddFetchArgs(
 			tkbtf.NewFetchArg("tid", "s32").FuncParamWithName("tsk", "pid").FuncParamWithName("tsk", "", "pid"),
@@ -138,9 +157,11 @@ func main() {
 	}
 
 	loadWakeUpNewTaskSymbol(symbolMap)
+	loadTaskStatsExitSymbol(symbolMap)
 
 	probesTracingMap := make(map[string]struct{})
 
+	seenBTFnames := make(map[string]interface{})
 	strippedBTFsCount := 0
 	err := filepath.Walk(btfHubArchiveRepoPath, func(path string, info fs.FileInfo, err error) error {
 		if !strings.HasSuffix(path, ".btf") {
@@ -190,6 +211,12 @@ func main() {
 		}
 		logger.Info("produced stripped spec", slog.String("path", strippedSpecPath), slog.Int("count", strippedBTFsCount))
 		strippedBTFsCount++
+
+		strippedSpecBase := filepath.Base(strippedSpecPath)
+		if _, exists := seenBTFnames[strippedSpecBase]; exists {
+			logger.Warn("name collision", slog.String("name", strippedSpecBase))
+		}
+		seenBTFnames[strippedSpecBase] = struct{}{}
 
 		if !validate {
 			return nil
